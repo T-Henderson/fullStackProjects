2ac1509c7c74d7a9570c0747e019fd8981edc17c
e-Henderson <eddie.henderson@revature.net>
2022-01-09 12:13:49 -0500

    Deleted best seller and book components
    Something went wrong when I changed the book-item to bestsellers and I ended up deleting those components. Going to rebuild it

--- bookhub/src/app/app.module.ts
+++ bookhub/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { BookItemComponent } from './components/book-item/book-item.component';
-import { BooksComponent } from './components/books/books.component';
 
 const appRoutes: Routes = [
   {path:'', component: HomepageComponent},//this should redirect to homepage
@@ -22,8 +20,7 @@ const appRoutes: Routes = [
     HomepageComponent,
     AboutComponent,
     FooterComponent,
-    BookItemComponent,
-    BooksComponent
+    
   ],
   imports: [
     BrowserModule,
