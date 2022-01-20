07439f21c547c0ab456d28b39eb7df0f6180a91e
e-Henderson <ethenderson15@gmail.com>
2022-01-19 19:35:37 -0500

    Update to login
    Added router to login component. Page is back, however "login works" isn't showing. Might be something in the navbar in app.component html

--- bookhub/src/app/app-routing.module.ts
+++ bookhub/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 
 export const appRoutes: Routes = [
   {path:'', component: HomepageComponent},//this should redirect to homepage
-  //{path:'/login', component: LoginComponent}
+  //{path:'/login', component: LoginComponent}//adding this makes the page go blank. Why?
   //{path: 'about', component: AboutComponent}//uncommenting this will bring back about
 ]
 
--- bookhub/src/app/components/login/login.component.ts
+++ bookhub/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.navigate(['/login']);//brings back the page. However, it's stuck on the homepage. the
+    //auot generated login works isn't showing up
   }
 
 }
